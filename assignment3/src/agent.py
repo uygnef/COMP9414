5e782dcfc5fe2159efa20941431d725131e2f577
Yu <97508691@qq.com>
2016-05-22 01:04:44 +1000

    hang_out done

--- assignment3/src/agent.py
+++ assignment3/src/agent.py
@@ -1,52 +1,76 @@
-
+import random
 from socket import *
 
 dirn = 3
-world = [['' for _ in range(80)]for _ in range(80)]
+world = [[' ' for _ in range(80)]for _ in range(80)]
 row = 40
 colum = 40
 stone = False
+prior_action = None
+have_been = set((colum,row))
+can_go = [' ','o']
+
+def go_to(point):
+    global dirn
+    global prior_action
 
 def hang_out(view):
     global dirn
     global stone
-    if view[1][2] == ' ' or view[1][2] == 'o':
-        if view[1][2] == 'o':
-            stone = True
-        action = 'f'
-        record_to_world()
-    elif view[2][3] == ' ' or view[2][3]== 'o' :
-        action = 'r'
-        if dirn - 1 < 0:
+    global prior_action
+    global can_go
+    # find the area that can go but have not been   
+    #stack = [] #save the dirction order by the prioritize
+
+    hang = []
+    if view[1][2] in can_go:
+        hang.append('f')
+        hang.append('f')
+        hang.append('f')
+        hang.append('f')
+        hang.append('f')
+        hang.append('f')
+
+    if view[2][3] in can_go:
+        hang.append('r')
+        hang.append('r')
+
+    if view[2][1] in can_go:
+        hang.append('l')
+
+    if len(hang)==0:
+        if dirn == 0:
             dirn = 3 
         else:
             dirn -= 1
-    else: 
-        action = 'l'
-        if dirn + 1 > 3:
+        prior_action = 'r'
+        return 'r'
+
+    action = random.sample(hang,1)[0]
+
+    if action == 'r':
+        if dirn == 0:
+            dirn = 3 
+        else:
+            dirn -= 1
+
+    elif action == 'l':
+        if dirn == 3:
             dirn = 0
         else:
             dirn += 1
+    prior_action = action
     return action
                  
 def get_view(data):
     # scan 5-by-5 wintow around current location
-    global view
     view = [[' ',' ',' ',' ',' ']for _ in range(5)]
-    print(len(data))
-    n = 0
-
-    while len(data) < 24:
-        a = 's'
-        sock.send(a.encode())
-        data = sock.recv(24).decode()
-        print(len(data))
-    
+    n = 0   
     for i in range(5):
        for j in range(5):             
          if not( i == 2 and j == 2 ):               
              view[i][j] = data[n]
-             n += 1         
+             n += 1
     return view
 
 
@@ -63,41 +87,39 @@ def print_view( view ):
     print("+-----+")
 
 
-def record_to_world(): #save the view into the world[][] which is world model
+def record_to_world(view): #save the view into the world[][] which is world model
 ##EAST   = 0;
 ##NORTH  = 1;
 ##WEST   = 2;
 ##SOUTH  = 3;
- global world
- global colum
- global row
- if dirn == 1:
-     c = -3
-     r = 0
- elif dirn == 3:
-     c = 3
-     r = 0
- elif dirn == 2:
-     c = 0
-     r = -3
- elif dirn == 0:
-     c = 0
-     r = 3	
-
- if r == 0:
-     colum = colum + c
-     print(dirn , colum ,c)
- if c == 0:
-     row += r 
-	
- for i in range(-2,3):
-    if r == 0:
-        world[colum][row+i] = view[0][2+i]
-        print(world[colum][row+i])
-    if c == 0:
-        world[colum + i][row] = view[0][2+i]
-        print(world[colum + i][row])
-
+    global world
+    global colum
+    global row
+    if dirn == 1:
+        c = -2
+        r = 0
+        colum -= 1
+    elif dirn == 3:
+        c = 2
+        r = 0
+        colum += 1
+    elif dirn == 2:
+        c = 0
+        r = -2
+        row -= 1
+    elif dirn == 0:
+        c = 0
+        r = 2
+        row += 1  
+    for i in range(-2,3):
+        if c == -2:
+            world[colum + c][row + i] = view[0][2+i]
+        elif c == 2:
+            world[colum + c][row - i] = view[0][2+i]
+        elif r == -2 :
+            world[colum - i][row + r] = view[0][2+i]
+        elif r == 2:
+            world[colum + i][row + r] = view[0][2+i]
 
 HOST = 'localhost'
 PORT = 31415
@@ -105,23 +127,17 @@ ADDR = (HOST, PORT)
 
 sock = socket(AF_INET, SOCK_STREAM)
 sock.connect(ADDR)
-a = 0
+data = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
+for i in range(-2,3):
+    for j in range(-2,3):
+            world[colum - i][row - j] = data[i+2][2+j]
+
 while True:
-    a = 0
-    data = sock.recv(1024)
-    data = get_view(sock.recv(1024).decode("UTF-8"))
-    print_view(data)
-    sock.send(hang_out(data).encode("UTF-8"))
-##    if len(data)>23:
-##        for i in range(5):
-##            for j in range(5):
-##                if i !=2 or j != 2:
-##                    print(data[a],end='')
-##                    a += 1
-##                else:
-##                    print('^',end='')
-##            print()
-    print()
+    if prior_action == 'f':
+        record_to_world(data)
+    action = hang_out(data)
+    sock.send(action.encode("UTF-8"))
+    data = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
+  # print_view(data)
 
 
-    
