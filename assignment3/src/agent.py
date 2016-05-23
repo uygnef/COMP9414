f588d6306aa34d6af61ac82e91178a1b119f9979
Yu <97508691@qq.com>
2016-05-24 00:56:59 +1000

    finished

--- assignment3/src/agent.py
+++ assignment3/src/agent.py
@@ -1,201 +1,47 @@
-import random
+#!/usr/bin/python3
+#! /usr/bin/env python3
+
+#Because the world is unknow,so I use breadthfirst search to make sure
+#the agent can get the gold. But it's a little slowly.
+#the program runs in 3 steps
+#1.dicovery: go to all boundary that haven't been, to get the biggest view
+#2.get_item: if agent see the item, and can go to(use find_path function) it
+#  the agent will go to there.if all the item have been get and no boundart
+#   haven't been, it will stop. Depend on the item it get to select the path.
+#3.if find gold, the agent go back to the startpoint. 
+
+# use chmod +x agent.py
+#    ./agent.py -p [Portnumber]
+#
+
 from socket import *
+import sys, getopt
 
-dirn = 3
-world = [[None for _ in range(80)]for _ in range(80)]
-row = 40
-colum = 40
-stone = False
-prior_action = None
-have_been = set((colum,row))
-can_go = [' ','o']
-axo = False
-gold = False
-key = False
+PORT = 21415 #for idle 
+ops, args = getopt.getopt(sys.argv[1:], "p:")
+for op , value in ops:
+    if op == "-p":
+        PORT = int(value)        
+        break
 
-class Node():
-    def __init__(self):
-        self.value = None
-        self.father = None
-        self.stone = False
-        self.colum = None
-        self.row = None
-        
-def search_path(end_colmn,end_row): #use breadth first search to find the right position
-    global colum
-    global row
-    global stone
-    global axo
-    global gold
-    
-    start = Node()
-    start.colum = colum
-    start.row = row
-    start.stone = stone
-    possibal_path = [start]
-    have_been = [(colum,row)]
-    for node in possibal_path:
-        can_go = [' ','o','k','a','g']
-        if node.colum == end_colmn and node.row == end_row:            
-            break
-        if node.stone:
-            can_go =  can_go + ['~']
-        if axo:
-            can_go = can_go + ['T']
-        if key:
-            can_go = can_go + ['-']
-        for i in [-1,1]:                     #breadth frist search check out every place where can go to
-            if world[node.colum+i][node.row] in can_go:
-                new_node = Node()
-                new_node.father = node
-                new_node.colum = node.colum+i
-                new_node.row = node.row
-                new_node.stone = node.stone
-                if world[node.colum+i][node.row] == '~':
-                    new_node.stone = False
-                if (new_node.colum,new_node.row) not in have_been:
-                    have_been.append((new_node.colum,new_node.row))
-                    possibal_path.append(new_node)
-
-            if world[node.colum][node.row+i] in can_go:
-                new_node = Node()
-                new_node.father = node
-                new_node.colum = node.colum
-                new_node.row = node.row+i
-                new_node.stone = node.stone
-                if world[node.colum][node.row+i] == '~':
-                    new_node.stone = False
-                if (new_node.colum,new_node.row) not in have_been:
-                    have_been.append((new_node.colum,new_node.row))
-                    possibal_path.append(new_node)
-    if node.colum != end_colmn or node.row != end_row: #search all possibal path can not get goal
-        return False
-    result = []
-    while node.father != None:
-        result.append([node.colum,node.row])       
-        node = node.father
-    return result 
-    
-
-def go_to(end):
-    global dirn
-    global prior_action
-    global colum
-    global row
-    print(prior_action)
-    a = colum - end[0]
-    b = row - end[1]
-    print('a={},b={},dirn={}'.format(a,b,dirn))
-    print('colum={},row={},end[0]={},end[1]={}'.format(colum,row,end[0],end[1]))
-    if a == -1:
-        if dirn == 3:
-            if world[end[0]][end[1]] == 'T':
-                prior_action = 'c'
-                return 'c'
-            if world[end[0]][end[1]] == '-':
-                prior_action = 'u'
-                return 'u'
-            prior_action = 'f'
-            return 'f'
-    if a == 1:
-        if dirn == 1:
-            if world[end[0]][end[1]] == 'T':
-                prior_action = 'c'
-                return 'c'
-            if world[end[0]][end[1]] == '-':
-                prior_action = 'u'
-                return 'u'
-            prior_action = 'f'
-            return 'f'
-    if b == 1:
-        if dirn == 2:
-            if world[end[0]][end[1]] == 'T':
-                prior_action = 'c'
-                return 'c'
-            if world[end[0]][end[1]] == '-':
-                prior_action = 'u'
-                return 'u'
-            prior_action = 'f'
-            return 'f'
-    if b == -1:
-        if dirn == 0:
-            if world[end[0]][end[1]] == 'T':
-                prior_action = 'c'
-                return 'c'
-            if world[end[0]][end[1]] == '-':
-                prior_action = 'u'
-                return 'u'
-            prior_action = 'f'
-            return 'f'
-    
-    if dirn == 0:
-        dirn = 3 
-    else:
-        dirn -= 1
-    prior_action = 'r'
-    return 'r'
-   
-
-def hang_out(view):
-    global dirn
-    global stone
-    global prior_action
-    global can_go
-    # find the area that can go but have not been   
-    #stack = [] #save the dirction order by the prioritize
-    hang = []
-    if view[1][2] in can_go:
-        for _ in range(3):
-            hang.append('f')
-
-    if view[2][3] in can_go:
-        hang.append('r')
-
-    if view[2][1] in can_go:
-        hang.append('l')
-
-    if len(hang)==0:
-        if dirn == 0:
-            dirn = 3 
-        else:
-            dirn -= 1
-        prior_action = 'r'
-        return 'r'
-
-    action = random.sample(hang,1)[0]
-
-    if action == 'r':
-        if dirn == 0:
-            dirn = 3 
-        else:
-            dirn -= 1
-
-    elif action == 'l':
-        if dirn == 3:
-            dirn = 0
-        else:
-            dirn += 1
-    prior_action = action
-    if action == 'f' and view[1][2] == 'o':
-        stone = True
-    return action
-                 
-def get_view(data):
-    # scan 5-by-5 wintow around current location
+def get_view(data):     # scan and get view from current location
     view = [[' ',' ',' ',' ',' ']for _ in range(5)]
     n = 0   
     for i in range(5):
        for j in range(5):             
          if not( i == 2 and j == 2 ):               
-             view[i][j] = data[n]
-             n += 1
+            try:
+                 view[i][j] = data[n]
+                 n += 1
+            except IndexError:
+                pass
+            
     return view
-
 
 def print_view( view ):
     print("\n+-----+")
     for i in range(5):
-        print("|",end = '')
+        print("|",point = '')
         for j in range(5):
             if i == 2 and j == 2 :
                print('^',end='')
@@ -204,29 +50,22 @@ def print_view( view ):
         print("|")     
     print("+-----+")
 
-
 def record_to_world(view): #save the view into the world[][] which is world model
-##EAST   = 0;
-##NORTH  = 1;
-##WEST   = 2;
-##SOUTH  = 3;
-    global world
+    global world    
     global colum
     global row
-    if dirn == 1:
+    global border_of_world 
+    c,r = None,None
+    if dirn == 1:       #NORTH  = 1
         c = -2
-        r = 0
         colum -= 1
-    elif dirn == 3:
+    elif dirn == 3:     #SOUTH  = 3
         c = 2
-        r = 0
         colum += 1
-    elif dirn == 2:
-        c = 0
+    elif dirn == 2:     #WEST   = 2
         r = -2
         row -= 1
-    elif dirn == 0:
-        c = 0
+    elif dirn == 0:     #EAST   = 0
         r = 2
         row += 1  
     for i in range(-2,3):
@@ -238,101 +77,273 @@ def record_to_world(view): #save the view into the world[][] which is world mode
             world[colum - i][row + r] = view[0][2+i]
         elif r == 2:
             world[colum + i][row + r] = view[0][2+i]
+    if colum - 2 < border_of_world['North']:
+        border_of_world['North'] = colum -2
+    if colum + 2 > border_of_world['South']:
+        border_of_world['South'] = colum + 2
+    if row - 2 < border_of_world['West']:
+        border_of_world['West'] = row - 2
+    if row + 2 > border_of_world['East']:
+        border_of_world['East'] = row + 2
+    if (colum, row) not in have_been:
+        have_been.add((colum, row))
+ 
+##----- direction operating function -------
+##0.revieve prior view[5][5] from parameter
+##1.judge whether can go straight and send command to server
+##2.recieve and return view[5][5]
+can_go = [' ','o']
 
-def target():      #search all item  from world map
-    result = {}
-    for i in range(30,60):
-        for j in range(30,60):
-            if world[i][j] == 'k':
-                result['k']=(i,j)
-            if world[i][j] == 'a':
-                result['a']=(i,j)
-            if world[i][j]== 'g':
-                result['g']=(i,j)
-            if world[i][j]== 'o':
-                result['o']=(i,j)
-    return result
+
+def turn_right():
+    global sock
+    global dirn
+    global view
+    
+    sock.send('r'.encode("UTF-8"))
+    if dirn == 0:
+        dirn = 3 
+    else:
+        dirn -= 1
+    view = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
+    return view
+
+
+def turn_left():
+    global sock
+    global dirn
+    
+    sock.send('l'.encode("UTF-8"))
+    if dirn == 3:
+        dirn = 0
+    else:
+        dirn += 1
+    view = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
+    return view
+    
+
+
+def forward():
+    global sock
+    global dirn
+
+    global view
+    global stone
+    global axo
+    global key
+    global gold
+
+    if view[1][2] == 'k':
+        key = True
+    if view[1][2] == 'o':
+        stone = True
+    if view[1][2] == 'a':
+        axo = True
+    if view[1][2] == 'g':
+        gold = True
+        sock.send('f'.encode("UTF-8"))
+        view = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
+        record_to_world(view)
+        go_to(search_path(80,80,stone))
+        exit()
+        
+    if view[1][2] == '~':
+        stone = False
+
+    if view[1][2] == 'T':
+        sock.send('c'.encode("UTF-8"))
+        view = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
+    if view[1][2] == '-':
+        sock.send('u'.encode("UTF-8"))
+        view = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
+##    if view[1][2] not in can_go:
+##        return False
+    
+    sock.send('f'.encode("UTF-8"))
+    view = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
+    record_to_world(view)
+    
+
+def discover():  #go to the border which have not been
+    from copy import deepcopy
+    global border_of_world
+    item = ['g','o','k','a']
+    prior = deepcopy(border_of_world)
+    for i in range(border_of_world['North'],border_of_world['South']):
+        for j in range(border_of_world['West'],border_of_world['East']):
+            if world[i][j] in item:
+                temp = search_path(i,j,False)
+                if temp :
+                    go_to(temp)
+            if (i,j) not in have_been and world[i][j] in can_go:
+                temp = search_path(i,j,False)
+                if temp:
+                    go_to(temp)
+                   
+    if  prior == border_of_world: # no more area to discover
+        return
+    else:
+        discover()
+#----------breadth first search-------
+class Node():
+    def __init__(self):
+        self.value = None
+        self.father = None
+        self.stone = False
+        self.colum = None
+        self.row = None
+
+    def inherit(self,node): #create new node Inherit from node
+        self.father = node
+        self.stone = node.stone
+        self.colum = node.colum
+        self.row = node.row
+
+def search_path(end_colmn,end_row,use_stone): #use breadth first search to find the right position
+    global colum
+    global row
+    global axo
+    global gold
+    global have_been
+
+    start = Node()
+    start.colum = colum
+    start.row = row
+    start.stone = use_stone
+    possibal_path = [start]
+    been = [(colum,row)]
+    for node in possibal_path:
+        can_go = [' ','o','k','a','g']
+
+        if node.colum == end_colmn and node.row == end_row:            
+            break
+        if node.stone:
+            can_go =  can_go + ['~']
+        if axo:
+            can_go = can_go + ['T']
+        if key:
+            can_go = can_go + ['-']
+        for i in [-1,1]:#breadth frist search check out every place where can go to
+            if world[node.colum+i][node.row] in can_go:
+                new_node = Node()
+                new_node.father = node
+                new_node.colum = node.colum+i
+                new_node.row = node.row
+                new_node.stone = node.stone
+
+                if world[node.colum+i][node.row] == '~':
+                    new_node.stone = False
+                if (new_node.colum,new_node.row) not in been:
+                    been.append((new_node.colum,new_node.row))
+                    possibal_path.append(new_node)
+
+            if world[node.colum][node.row+i] in can_go:
+                new_node = Node()
+                new_node.father = node
+                new_node.colum = node.colum
+                new_node.row = node.row+i
+                new_node.stone = node.stone
+                if world[node.colum][node.row+i] == '~':
+                    new_node.stone = False
+                if (new_node.colum,new_node.row) not in been:
+                    been.append((new_node.colum,new_node.row))
+                    possibal_path.append(new_node)
+    if node.colum != end_colmn or node.row != end_row: #search all possibal path can not get goal
+        return False
+    result = []
+    
+    while node.father != None:
+        result.append([node.colum,node.row])
+        if not (node.colum,node.row) in have_been:
+            have_been.add((node.colum,node.row))
+        node = node.father
+    result.reverse()
+    return result 
+    
+
+def go_to(result):   # follow the search path go to destination
+    global dirn
+    global prior_action
+    global colum
+    global row
+
+    for point in result:
+        a = colum - point[0]
+        b = row - point[1]
+       # print('a={},b={},dirn={}'.format(a,b,dirn))
+       # print('colum={},row={},point[0]={},point[1]={}'.format(colum,row,point[0],point[1]))
+        if a == -1:
+            while dirn != 3:
+                turn_right()
+            forward()
+                
+        elif a == 1:
+            while dirn != 1:
+                turn_right()
+            forward()
+        elif b == 1:
+            while dirn != 2:
+                turn_right()
+            forward()
+        elif b == -1:
+            while dirn != 0:
+                turn_right()
+            forward()
+    return
+#-------find and get item--------
+def get_item():
+    global stone
+    item = ['g','o','k','a']
+    goal = []
+    for i in range(border_of_world['North']-5,border_of_world['South']+5):
+        for j in range(border_of_world['West']-5,border_of_world['East']+5):
+            if world[i][j] in item:
+                temp = search_path(i,j,stone)
+                if temp:
+                    goal.append(temp)
+    if len(goal) == 0:
+        return
+    for p in goal:
+        go_to(p)
+    
+    
+                    
+                    
+
+
+#--------------------------------    
+
+
+##-----global, record current row and colum in world.
+##dirn is direction same in record_world()
+axo = False
+key = False
+gold = False
+stone = False
+colum = 80
+row = 80
+dirn = 3
+world = [[None for _ in range(160)]for _ in range(160)]
+#the border of the world have been discoverd
+border_of_world = {'West':78,'North':78,'South':82,'East':82}
+#record the area '^' have been
+have_been = set((colum,row))
 
 
 HOST = 'localhost'
-PORT = 31415
+
 ADDR = (HOST, PORT) 
 
 sock = socket(AF_INET, SOCK_STREAM)
 sock.connect(ADDR)
-data = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
-for i in range(-2,3):
-    for j in range(-2,3):
-            world[colum - i][row - j] = data[i+2][2+j]
+view = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
+for i1 in range(-2,3):
+    for j1 in range(-2,3):
+            world[colum - i1][row - j1] = view[i1+2][2+j1]
+while True:
+    discover()
+    get_item()
 
-for _ in range(300):
-    action = hang_out(data)
-    sock.send(action.encode("UTF-8"))
-    data = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
-    if prior_action == 'f':
-        record_to_world(data)
-
-
-profile={'o':axo,'g':gold,'k':key,'o':stone}
-destination = {}
-result = target()
-for a in result:
-    track = search_path(result[a][0],result[a][1]) #all the possibal way to get item
-    if track:
-        if a == 'k':
-            key = True
-        if a == 'g':
-            gold == True
-        if a == 'o':
-            axo = True
-        destination[a] = track
+    
+    
         
-
-for p in destination:
-    path = destination[p]
-    while len(path) > 0:
-        print(path)
-        des = path.pop()
-        action = go_to(des)
-        sock.send(action.encode("UTF-8"))
-        data = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
-        if prior_action == 'f':
-            record_to_world(data)
-        if des[0] == result[p][0] and des[1] == result[p][1]:
-            break
-        if prior_action != 'f':
-            path.append(des)
-
-for _ in range(50):
-    action = hang_out(data)
-    sock.send(action.encode("UTF-8"))
-    data = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
-    if prior_action == 'f':
-        record_to_world(data)
-
-destination = {}
-result = target()
-for a in result:
-    track = search_path(result[a][0],result[a][1]) #all the possibal way to get item
-    if track:
-        destination[a] = track
-
-for p in destination:
-    path = destination[p]
-    while len(path) > 0:
-        print(path)
-        des = path.pop()
-        action = go_to(des)
-        sock.send(action.encode("UTF-8"))
-        data = get_view(sock.recv(24,MSG_WAITALL).decode("UTF-8"))
-        if prior_action == 'f':
-            record_to_world(data)
-        if des[0] == result[p][0] and des[1] == result[p][1]:
-            break
-        if prior_action != 'f':
-            path.append(des)
-    
-    
-  # print_view(data)
-
-
